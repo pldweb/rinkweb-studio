9fb76f950fcd145eec0956d8538b3ece9ce0ccfc
Muhammad Rivaldi Fanani <muhammadrivaldifnni01@gmail.com>
2025-09-04 00:03:25 +0700

    huhu

--- backend/api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Vercel serverless function entry point for Laravel
-
-// Set the correct path for Laravel
-$_SERVER['SCRIPT_NAME'] = '/index.php';
-
-// Include the Laravel public index file
-require __DIR__ . '/../public/index.php';
